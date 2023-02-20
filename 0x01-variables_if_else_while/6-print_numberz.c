5b2c2c57ca381eb6c13d28596372b4f9c2d1cbce
Wilson <wilson.mungai.muthui@gmail.com>
2023-02-20 03:03:55 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints all numbers starting
+ * from base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
